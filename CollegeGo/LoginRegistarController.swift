0a22ef16cd9d2eb764b47080810feaf1adb24bbf
Mario Hernandez <mhmario963@gmail.com>
2017-01-13 23:24:21 -0700

    Test

---
+++ CollegeGo/LoginRegistarController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollegeGo
+//
+//  Created by Mario Hernandez on 1/13/17.
+//  Copyright © 2017 Mario Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class LoginRegisterController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
